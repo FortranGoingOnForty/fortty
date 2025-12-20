0cc7ca5f212414072059df548627fce9f016f4e6
mfwolffe <wolffemf@dukes.jmu.edu>
2025-12-20 13:09:21 -0500

    Add configurable color palette to cell module
    Add custom 16-color palette support:
    - set_palette_color: override individual palette entries
    - set_default_colors: set default foreground/background
    - init_default_palette: initialize with VGA-style defaults
    
    Modify color_from_index to use custom palette for indices 0-15,
    allowing configuration-driven terminal theming.

--- src/terminal/cell.f90
+++ src/terminal/cell.f90
@@ -8,6 +8,7 @@ module cell_mod
   public :: ATTR_INVERSE, ATTR_HIDDEN, ATTR_STRIKETHROUGH
   public :: default_fg, default_bg, make_cell
   public :: color_from_index, color_from_rgb
+  public :: set_palette_color, set_default_colors
 
   ! Color modes
   integer, parameter :: COLOR_DEFAULT = 0
@@ -36,9 +37,14 @@ module cell_mod
     integer :: attrs = 0              ! Attribute flags
   end type cell_t
 
-  ! Default colors
-  type(color_t), parameter :: default_fg = color_t(COLOR_DEFAULT, 7, 255, 255, 255)
-  type(color_t), parameter :: default_bg = color_t(COLOR_DEFAULT, 0, 0, 0, 0)
+  ! Default colors (can be overridden by config)
+  type(color_t) :: default_fg = color_t(COLOR_DEFAULT, 7, 255, 255, 255)
+  type(color_t) :: default_bg = color_t(COLOR_DEFAULT, 0, 0, 0, 0)
+
+  ! Configurable 16-color palette (indexed 0-15)
+  ! Can be overridden via set_palette_color
+  type(color_t), save :: custom_palette(0:15)
+  logical, save :: palette_initialized = .false.
 
 contains
 
@@ -55,31 +61,47 @@ contains
     c%attrs = attrs
   end function make_cell
 
+  ! Initialize custom palette with default VGA colors
+  subroutine init_default_palette()
+    integer :: i
+    ! VGA palette RGB values
+    integer, parameter :: vga_r(0:15) = [0, 128, 0, 128, 0, 128, 0, 192, 128, 255, 0, 255, 0, 255, 0, 255]
+    integer, parameter :: vga_g(0:15) = [0, 0, 128, 128, 0, 0, 128, 192, 128, 0, 255, 255, 0, 0, 255, 255]
+    integer, parameter :: vga_b(0:15) = [0, 0, 0, 0, 128, 128, 128, 192, 128, 0, 0, 0, 255, 255, 255, 255]
+
+    do i = 0, 15
+      custom_palette(i) = color_t(COLOR_RGB, i, vga_r(i), vga_g(i), vga_b(i))
+    end do
+    palette_initialized = .true.
+  end subroutine init_default_palette
+
+  ! Set a single palette color (index 0-15)
+  subroutine set_palette_color(idx, c)
+    integer, intent(in) :: idx
+    type(color_t), intent(in) :: c
+
+    if (.not. palette_initialized) call init_default_palette()
+    if (idx >= 0 .and. idx <= 15) then
+      custom_palette(idx) = c
+    end if
+  end subroutine set_palette_color
+
+  ! Set default foreground and background colors
+  subroutine set_default_colors(fg, bg)
+    type(color_t), intent(in) :: fg, bg
+
+    default_fg = fg
+    default_bg = bg
+  end subroutine set_default_colors
+
   ! Create color from 256-color palette index
   function color_from_index(idx) result(c)
     integer, intent(in) :: idx
     type(color_t) :: c
     integer :: r, g, b, gray
 
-    ! Standard 16 colors (VGA palette)
-    integer, parameter :: palette(0:15, 3) = reshape([ &
-      0,   0,   0,   &  ! 0  Black
-      128, 0,   0,   &  ! 1  Red
-      0,   128, 0,   &  ! 2  Green
-      128, 128, 0,   &  ! 3  Yellow
-      0,   0,   128, &  ! 4  Blue
-      128, 0,   128, &  ! 5  Magenta
-      0,   128, 128, &  ! 6  Cyan
-      192, 192, 192, &  ! 7  White
-      128, 128, 128, &  ! 8  Bright Black (Gray)
-      255, 0,   0,   &  ! 9  Bright Red
-      0,   255, 0,   &  ! 10 Bright Green
-      255, 255, 0,   &  ! 11 Bright Yellow
-      0,   0,   255, &  ! 12 Bright Blue
-      255, 0,   255, &  ! 13 Bright Magenta
-      0,   255, 255, &  ! 14 Bright Cyan
-      255, 255, 255  &  ! 15 Bright White
-    ], [16, 3])
+    ! Initialize palette on first use if needed
+    if (.not. palette_initialized) call init_default_palette()
 
     c%mode = COLOR_RGB
     c%index = idx
@@ -88,10 +110,8 @@ contains
       ! Invalid index - return white
       c%r = 255; c%g = 255; c%b = 255
     else if (idx < 16) then
-      ! Standard 16 colors
-      c%r = palette(idx, 1)
-      c%g = palette(idx, 2)
-      c%b = palette(idx, 3)
+      ! Use custom palette for standard 16 colors
+      c = custom_palette(idx)
     else if (idx < 232) then
       ! 6x6x6 color cube (indices 16-231)
       ! index = 16 + 36*r + 6*g + b where r,g,b in 0-5
